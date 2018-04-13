883e752440ebcc1800a4cb5e0ae7fb413ff9461f
Jose Mario Gutierrez <jogutierrez@H7689.ad.hugeinc.com>
2018-04-13 15:05:55 -0500

    Initial files

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const fs = require('fs');
+
+var server = http.createServer(
+    function(req,res) {
+  
+        if (req.url === '/home' || req.url === '/') {
+            res.writeHead(200, {'Content-type' : 'text/html'});
+            fs.createReadStream(__dirname + '/index.html','utf8').pipe(res);
+        
+        } 
+    }
+ );
+
+ server.listen(4000,'127.0.0.1');
